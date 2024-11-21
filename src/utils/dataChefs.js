92f7cbf2bd71d8d4f997cf0e691066ecba03af21
Adriyansah Syaputra <adriansyahputra2626@gmail.com>
2024-11-21 14:54:17 +0700

    fix link pages

---
+++ src/utils/dataChefs.js
@@ -0,0 +1,37 @@
+const chefs = [
+  {
+    id: 1,
+    name: "Michael Gordon",
+    role: "Executive Chef",
+    image: "./public/img/chef/1.jpg",
+    socials: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://www.instagram.com",
+      twitter: "https://www.twitter.com",
+    },
+  },
+  {
+    id: 2,
+    name: "Francisca Veronika",
+    role: "Sous Chef",
+    image: "./public/img/chef/2.jpg",
+    socials: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://www.instagram.com",
+      twitter: "https://www.twitter.com",
+    },
+  },
+  {
+    id: 3,
+    name: "Joseph Calvin",
+    role: "Pastry Chef",
+    image: "./public/img/chef/3.jpg",
+    socials: {
+      facebook: "https://www.facebook.com",
+      instagram: "https://www.instagram.com",
+      twitter: "https://www.twitter.com",
+    },
+  },
+];
+
+export { chefs };
